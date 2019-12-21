f89190b465b6cf65465175c57470c6cda276e018
Alice Alsford <alice.alsford@gmail.com>
2019-12-21 15:10:46 +1300

    basic fizzbuzz working

--- client/components/FizzBuzz.jsx
+++ client/components/FizzBuzz.jsx
@@ -4,34 +4,42 @@ class FizzBuzz extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            word: ""
+            wordArray: []
         }
     }
 
     produceFizzBuzz = () => {
-        console.log("in here")
-        console.log(this.state)
-        for (let i = 0; i <= 100; i++) {
+        let thisWordArray = []
+
+        for (let i = 1; i <= 100; i++) {
             if (i % 3 === 0 && i % 5 === 0) {
-                this.setState({ word: "FizzBuzz" })
+                console.log("fizzbuzz")
+                thisWordArray.push("fizzbuzz")
             } else if (i % 3 === 0) {
-                this.setState({ word: "Fizz" })
+                console.log("fizz")
+                thisWordArray.push("fizz")
             } else if (i % 5 === 0) {
-                this.setState({ word: "Buzz" })
+                console.log("buzz")
+                thisWordArray.push("buzz")
             } else {
-                this.setState({ word: i })
+                console.log(i)
+                thisWordArray.push(i)
             }
         }
+        this.setState({ wordArray: thisWordArray })
     }
 
 
     render() {
+        console.log(this.state)
         return (
             <React.Fragment>
                 <h2>This will be the fizz buzz </h2>
 
                 <button onClick={this.produceFizzBuzz}>Click for FizzBuzz</button>
-                <h3>{this.state.word}</h3>
+                <ul> {this.state.wordArray.map((word, i) => {
+                    return <li key={i}>{word}</li>
+                })}</ul>
             </React.Fragment>
         )
     }
