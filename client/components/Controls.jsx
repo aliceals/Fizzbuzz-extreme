f0eeb71dfd2bbcd7369d945b616d22d82f3c62a6
Alice Alsford <alice.alsford@gmail.com>
2019-12-23 13:55:44 +1300

    add own version of fizzbuzz

--- client/components/Controls.jsx
+++ client/components/Controls.jsx
@@ -4,21 +4,21 @@ class Controls extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            wordArray: [],
-            toggleFizzBuzz: false
+            toggleFizzBuzz: false,
+            wordArray: []
         }
     }
 
-    produceAbletech = () => {
+    produceNewFizzbuzz = () => {
         let thisWordArray = []
 
         for (let i = 1; i <= 100; i++) {
             if (i % 3 === 0 && i % 5 === 0) {
-                thisWordArray.push("Abletech")
+                thisWordArray.push(this.state.first + " " + this.state.second)
             } else if (i % 3 === 0) {
-                thisWordArray.push("Able")
+                thisWordArray.push(this.state.first)
             } else if (i % 5 === 0) {
-                thisWordArray.push("tech")
+                thisWordArray.push(this.state.second)
             } else {
                 thisWordArray.push(i)
             }
@@ -30,26 +30,36 @@ class Controls extends React.Component {
 
     }
 
-    removeAbletech = () => {
-        this.setState({ wordArray: ["Abletech is the best"], toggleFizzBuzz: false })
+    removeNewFizzbuzz = () => {
+        this.setState({ wordArray: ["Bye Bye"], toggleFizzBuzz: false })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.produceNewFizzbuzz()
     }
 
 
     render() {
         return (
             <React.Fragment>
-                {this.state.toggleFizzBuzz == false && <button onClick={this.produceAbletech}>Wonder what this button does?</button>}
-                {this.state.toggleFizzBuzz && <button onClick={this.removeAbletech}>Delete Abletech</button>}
-                <h4>Add three words</h4>
-                <form>
-                    <label htmlFor="first">First word</label>
-                    <input type="string"></input>
+
+                <h3>Make your own version of Fizzbuzz</h3>
+                <h4>Add two words</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="first" >First word</label>
+                    <input name="first" onChange={this.handleChange} type="string"></input>
                     <label htmlFor="second">Second word</label>
-                    <input type="string"></input>
-                    <label htmlFor="third">Third word</label>
-                    <input type="string"></input>
+                    <input name="second" onChange={this.handleChange} type="string"></input>
                     <button type="submit">Submit</button>
                 </form>
+                {this.state.toggleFizzBuzz && <button onClick={this.removeNewFizzbuzz}>Delete</button>}
 
                 <ul> {this.state.wordArray.map((word, i) => {
                     return <li key={i} > {word}</li>
