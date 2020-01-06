d6b3074819aba1dcfd788b1f81224718e3fdd767
Alice Alsford <alice.alsford@gmail.com>
2020-01-06 18:48:49 +1300

    buzz the words

--- src/FizzBuzz.jsx
+++ src/FizzBuzz.jsx
@@ -36,9 +36,9 @@ class FizzBuzz extends React.Component {
                 <h2>Original fizzbuzz</h2>
                 {this.state.toggleFizzBuzz === false && <button onClick={this.produceFizzBuzz}>Click for FizzBuzz</button>}
                 {this.state.toggleFizzBuzz && <><button onClick={this.removeFizzBuzz}>Delete FizzBuzz</button> <button onClick={this.toggleAnimate}>Animate</button></>}
-                {this.state.animate && <ul> {this.state.wordArray.map((word, i) => {
+                {this.state.animate && <><h3>Hover over Fizz, Buzz and FizzBuzz</h3><ul> {this.state.wordArray.map((word, i) => {
                     return <li key={i} class={word}> {word}</li>
-                })}</ul>}
+                })}</ul> </>}
                 {this.state.animate === false && <ul> {this.state.wordArray.map((word, i) => {
                     return <li key={i}> {word}</li>
                 })}</ul>}
